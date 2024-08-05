98fb7e740d8939b2b66eedcd3eb4e8146403c9b8
chandrika dasari <114644226+ChandrikaDasari@users.noreply.github.com>
2024-08-05 16:58:06 +0530

    Add files via upload

--- binary.cpp
+++ binary.cpp
@@ -4,27 +4,23 @@ using namespace std;
 
 
 // basic iterative approach where time complexity is O(log n)
-class solution{
-public:
-void bs(vector<int>&arr,int target,int n){
+int bs(vector<int>&arr,int target,int n){
     int n=arr.size();
-    int low=0,high=n-1;
+    int low=0;
+    int high=n-1;
     while(low<=high){
         int mid=(low+high)/2;
-        if(mid[arr]==target) return mid;
-        else if(target>mid[arr]) return low=mid+1;
+        if(arr[mid]==target) return mid;
+        else if(target>arr[mid]) return low=mid+1;
         else return high=mid-1;
     }
     return -1;
 }
-};
 int main(){
     vector<int>a ={1,2,3,4,5,6};
     int n=a.size();
-    for(int i=0;i<a.size();i++){
-        cin>>a[i];
-    }
-    cout<<"element found at:";
-    bs(a,n)<<endl;
+    cin>>a[i]>>n;
+    
+    cout<<"element found at:"<<bs(a,n)<<endl;
     return 0;
 }
